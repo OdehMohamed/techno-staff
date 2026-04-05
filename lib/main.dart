273834ddba5a8e5b469cc48c3201c5cc367b8d81
OdehMohamed <moh95od@gmail.com>
2026-04-05 12:30:39 +0300

    feat(firebase): configure Firebase for Android, iOS, and web

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'app/app.dart';
 import 'core/theme/cubit/theme_cubit.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(
     EasyLocalization(
